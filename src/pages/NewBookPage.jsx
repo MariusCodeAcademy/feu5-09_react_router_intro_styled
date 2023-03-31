10bcd3cc5913e892d8b3e4d7051f7fdfd6230f9e
Marius CA <mariuskcodeacademy@gmail.com>
2023-03-31 10:58:57 +0300

    NewBookForm separate

--- src/pages/NewBookPage.jsx
+++ src/pages/NewBookPage.jsx
@@ -1,75 +1,13 @@
 import React from 'react';
-import { useState } from 'react';
+import NewBookForm from '../components/booksComponents/NewBookForm';
 
 function NewBookPage() {
-  const [title, setTitle] = useState('');
-  const [author, setAuthor] = useState('');
-  const [year, setYear] = useState('');
-  const [genre, setGenre] = useState('');
-  const [category, setCategory] = useState('');
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    const book = { title, author, year, genre, category };
-    // createBook(book);
-    setTitle('');
-    setAuthor('');
-    setYear('');
-    setGenre('');
-    setCategory('');
-  }
   return (
-    <form className="page container" onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="title">Title:</label>
-        <input
-          type="text"
-          id="title"
-          value={title}
-          onChange={(event) => setTitle(event.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="author">Author:</label>
-        <input
-          type="text"
-          id="author"
-          value={author}
-          onChange={(event) => setAuthor(event.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="year">Year:</label>
-        <input
-          type="text"
-          id="year"
-          value={year}
-          onChange={(event) => setYear(event.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="genre">Genre:</label>
-        <input
-          type="text"
-          id="genre"
-          value={genre}
-          onChange={(event) => setGenre(event.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="category">Category:</label>
-        <select
-          id="category"
-          value={category}
-          onChange={(event) => setCategory(event.target.value)}
-        >
-          <option value="">Select a category</option>
-          <option value="Fiction">Fiction</option>
-          <option value="Non-fiction">Non-fiction</option>
-        </select>
-      </div>
-      <button type="submit">Add Book</button>
-    </form>
+    <div className="container page">
+      <h1>NewBookPage</h1>
+      <p>Welcome to NewBookPage page</p>
+      <NewBookForm />
+    </div>
   );
 }
 
